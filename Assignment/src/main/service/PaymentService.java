9e4742a6c1f0094f072ecba1d45344dcfd4ed9c0
Duc Nguyen <38359443+ducnguyen55@users.noreply.github.com>
2024-05-27 13:40:31 +0700

    Update PaymentService.java

--- Assignment/src/main/service/PaymentService.java
+++ Assignment/src/main/service/PaymentService.java
@@ -19,20 +19,4 @@ public class PaymentService {
             System.out.println("Sorry! Not enough funds to proceed with payment.");
         }
     }
-
-    public void payBills(Customer customer, List<Bill> bills) {
-        long totalAmount = bills.stream().mapToLong(Bill::getAmount).sum();
-        if (customer.getBalance() >= totalAmount) {
-            for (Bill bill : bills) {
-                customer.setBalance(customer.getBalance() - bill.getAmount());
-                bill.setState(BillState.PAID);
-                Payment payment = new Payment(bill.getAmount(), LocalDate.now(), PaymentState.PROCESSED, bill.getBillId());
-                customer.getPayments().add(payment);
-            }
-            System.out.println("Payment completed for all bills.");
-            System.out.println("Your current balance is: " + customer.getBalance());
-        } else {
-            System.out.println("Sorry! Not enough funds to proceed with payment.");
-        }
-    }
 }
